82f375bccda5689daf31b27b1edb5a1fe66c2c0a
Kamil Gulzar <kamilgulzar289@gmail.com>
2023-10-15 11:31:23 -0700

    update
    update

---
+++ app.js
@@ -0,0 +1,81 @@
+// Chapter 1 Alerts
+
+// Q1 
+// alert("Hello World")
+
+//Q2
+// alert("Error! Please enter a valid password")
+
+//Q3
+// alert("Welcome to JS Land..\n Happy Coding")
+
+// Q4
+// alert("Welcome to JS Land..")
+// alert("Happy Coding")
+
+// Q5
+// successfully checked on the console
+
+// Q6
+// Used while linking HTML file
+
+// Q7
+{/* <script src="app.js"></script> */}
+
+// Chapter 2 Variable for strings
+
+// Q1
+// var username = "kamil_1"
+
+// Q2
+// var myName = "Kamil Gulzar"
+
+// Q3
+// var message = "Hello World"
+// alert(message)
+
+// Q4
+// var myName = "Jhone Doe"
+// var age = "15 Years Old"
+// var profession="Certified Mobile Application Development"
+// alert(myName)
+// alert(age)
+// alert(profession)
+
+// Q5
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza)
+
+// Q6
+// var email = "example@example.com"
+// alert("My Email is" + " " + email)
+
+// Q7
+// var Book = "A smarter to learn JavaScript"
+// alert("I am trying to learn from the book"+" "+Book)
+
+// Q8
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// Q9
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+
+// Chapter 3 Variables for Numbers
+
+// Q1
+// var age = 15
+// alert("I am" " "+ age + "Old")
+
+// Q2
+// skipped
+
+// Q3
+// var birthYear = 1990
+// document.write("My birth Year is" + " " + birthYear)
+// document.write("\n Data type of my declared variable is number")
+
+// Q4
+// document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store")
+
+// Chapter 4
